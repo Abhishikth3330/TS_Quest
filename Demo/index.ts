2884f9b80ff11ab4b3f4130bcb3a0a1e9ac26620
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2024-07-13 21:53:57 +0530

    added the index.ts in demo folder and updated pizza app

---
+++ Demo/index.ts
@@ -0,0 +1,19 @@
+let myName: string = "Bob"
+
+// another way to write the above code
+
+// let myName = "Bob"
+
+// Type 'number' is not assignable to type 'string'
+// myName = 5
+
+// Challenge:
+// Primitive Data Types: string, number, boolean
+// we have constuctors such as - String, Number and Boolean
+
+// first letter uppercase and lowercase is the difference
+// challenge: Explicitly type the variables below:
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
+
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const menu = [
-    { name : "Margherita", price : 8},
-    { name : "Pepperoni", price : 10},
-    { name : "Hawaiian", price : 10},
-    { name : "Veggie", price : 9},
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
 ]
 
 let cashInRegister = 100
@@ -15,29 +15,35 @@ function addNewPizza(pizzaObj) {
 
 function placeOrder(pizzaName) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
-    if (!selectedPizza){
+    if (!selectedPizza) {
         console.error(`${pizzaName} does not exist in the menu`)
         return
     }
     cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza : selectedPizza, status: "ordered"}
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
     return newOrder
 }
 
-function completeOrder(orderID) {
-    const order = orderQueue.find(order => order.id === orderID)
+/**
+ * Challenge: Teach TS what data type should be used for the 
+ * orderId in the completeOrder function. Then check for any
+ * additional warnings TS comes up with and fix those.
+ */
+
+function completeOrder(orderId: number) {
+    const order = orderQueue.find(order => order.id === orderId)
     order.status = "completed"
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12})
-addNewPizza({ name: "BBQ Chicken", cost: 12})
-addNewPizza({ name: "Spicy Sausage", cost: 11})
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
 
 placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
+completeOrder(1)
 
-console.log("Menu : ", menu)
-console.log("Cash in Register : ", cashInRegister)
-console.log("Order Queue : ", orderQueue)
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
